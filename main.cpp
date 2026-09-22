d74549210f64e9e98ed599ca6244afdbcd356906
Pavlo Shutz <pavlo050706@gmail.com>
2026-09-22 08:24:42 +0300

    Add client connection, port arg, IPv6 support, cleanup
    Refactor to connect as a client to www.example.com using a specified or default port. Add --port argument parsing, switch addrinfo hints to AF_UNSPEC for IPv4/IPv6 support, and improve string handling. Remove ListenSocket and related server code. Log selected port to stdout and handle connection errors and cleanup.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,10 @@
 //	Build:
 //		Link with Ws2_32.lib
 
+#ifndef UNICODE
+#define UNICODE
+#endif
+
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
@@ -19,15 +23,32 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
+#include <string>
 #include <string_view>
 
 #pragma comment(lib, "Ws2_32.lib")
 
 constexpr int DEFAULT_BUFLEN = 1 << 13;
-constexpr std::string_view DEFAULT_PORT = "27015";
+constexpr std::string_view DEFAULT_PORT = "48012";
 
 int __cdecl main(int argc, char **argv) {
+	std::string port{};
+
+	// Read command line arguments
+	for (int i = 1; i < argc && argv[i] != nullptr; ++i) {
+		if (strcmp(argv[i], "--port") == 0) {
+			if (argv[i + 1] != nullptr) {
+				port = argv[i + 1];
+			}
+			else {
+				std::cerr << "error: missing port number" << std::endl;
+				return EXIT_FAILURE;
+			}
+		}
+	}
+
 	WSADATA wsaData{};
 	int iResult{};
 
@@ -46,25 +67,53 @@ int __cdecl main(int argc, char **argv) {
 		return EXIT_FAILURE;
 	}
 
-	SOCKET ListenSocket = INVALID_SOCKET;
-
 	struct addrinfo *result{ nullptr };
 	struct addrinfo hints {};
 
 	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET;
+	// TODO: Handle IPv6 properly
+	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE;
+	//hints.ai_flags = AI_PASSIVE;
 
 	// Resolve the server address and port
-	iResult = getaddrinfo(nullptr, (argc > 1 ? argv[1] : DEFAULT_PORT.data()), &hints, &result);
+	// TODO: Handle --port <number>
+	iResult = getaddrinfo("www.example.com",
+		port.empty() ? DEFAULT_PORT.data() : port.c_str(), &hints, &result);
 	if (iResult != 0) {
 		std::cerr << "getaddrinfo failed: " << iResult << std::endl;
 		WSACleanup();
 		return EXIT_FAILURE;
 	}
 
+	// TODO: Maybe add logging instead of standart I/O
+	std::cout << "Set caching proxy server port to " << DEFAULT_PORT << std::endl;
+
+	SOCKET ConnectSocket = INVALID_SOCKET;
+	struct addrinfo *ptr{ nullptr };
+
+	for (ptr = result; ptr != nullptr; ptr = ptr->ai_next) {
+		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+		if (ConnectSocket == INVALID_SOCKET) {
+			std::cerr << "socket failed with error: " << WSAGetLastError() << std::endl;
+			freeaddrinfo(result);
+			WSACleanup();
+			return 1;
+		}
+
+		iResult = connect(ConnectSocket, ptr->ai_addr, static_cast<int>(ptr->ai_addrlen));
+		if (iResult == SOCKET_ERROR) {
+			closesocket(ConnectSocket);
+			ConnectSocket = INVALID_SOCKET;
+			continue;
+		}
+
+		break; // connection established
+	}
+
+	freeaddrinfo(result);
+	closesocket(ConnectSocket);
 	WSACleanup();
 
 	return EXIT_SUCCESS;
